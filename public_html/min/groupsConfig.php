28d586ed6310335680a7ec9ca723ab26aac8ea8e
esparreboom <eveline@kominski.net>
2013-10-08 11:24:43 +0200

    bxslider

--- public_html/min/groupsConfig.php
+++ public_html/min/groupsConfig.php
@@ -8,9 +8,7 @@
  */
 
 $aJsLibrary = array(
-//	'jquery'					=> '//js/library/jquery-1.6.4.min.js',
 	'jquery'					=> '//js/library/jquery-1.7.2.min.js',
-//	'jqueryUi'					=> '//js/library/jquery-ui-1.8.16.custom.min.js',
 	'jqueryUi'					=> '//js/library/jquery-ui-1.8.20.custom.min.js',
 	'kwglCore'					=> '//js/library/kwgl/core.js',
 	'kwglMessage'				=> '//js/library/kwgl/message.js',
@@ -19,13 +17,14 @@ $aJsLibrary = array(
 	'kwglPagination'			=> '//js/library/kwgl/pagination.js',
 	'kwglTranslate'				=> '//js/library/kwgl/translate.js',
 	'kwglUtility'				=> '//js/library/kwgl/utilities.js',
-    'bootstrap'                 =>  '//js/library/bootstrap.min.js'
+    'bootstrap'                 =>  '//js/library/bootstrap.min.js',
+    'bx'                        =>  '//js/library/jquery.bxslider.min.js'
 );
 $aJsPlugins = array(
 	'jsViews' => array(
 		'jsrender'				=> '//js/library/jsviews/jsrender.js',
 		'observable'			=> '//js/library/jsviews/jquery.observable.js',
-		'views'					=> '//js/library/jsviews/jquery.views.js',
+		'views'					=> '//js/library/jsviews/jquery.views.js'
 	),
 );
 $aCssBase = array(
@@ -35,26 +34,19 @@ $aCssBase = array(
 	'baseGrid-24'				=> '//css/_base/grid-24.css',
 	'baseGrid-Fraction-Fixed'	=> '//css/_base/grid-fraction-fixed.css',
 	'baseGrid-Fraction-Fluid'	=> '//css/_base/grid-fraction-fluid.css',
-	'baseStyle'					=> '//css/_base/style.css',
+	'baseStyle'					=> '//css/_base/style.css'
 );
 $aCssLibrary = array(
-//	'jqueryUi'					=> '//css/library/jquery-ui-1.8.16.custom.css',
-//	'jqueryUi'					=> '//css/library/jquery-ui-1.8.20.custom.css',
 	'bootstrap'					=> '//css/library/bootstrap/bootstrap.css',
-	'style'					=> '//css/default/style.css',
+	'bx'					=> '//css/library/jquery.bxslider.css',
+	'style'					=> '//css/default/style.css'
 );
 
 return array(
-
 	'jsCore'		=> $aJsLibrary,
 	'jsViews'		=> $aJsPlugins['jsViews'],
-
 	'cssBase'		=> $aCssLibrary,
-
-//	'cssBaseWithBootstrap' => array_merge($aCssBase, ),
-
-	//default module
 	'jsExamples'	=> array('//js/default/ajax-example.js'),
-	'cssMain'		=> array($aCssLibrary['jqueryUi'], '//css/default/ui_extender.css', '//css/default/js-examples.css')
+	'cssMain'		=> array($aCssBase, '//css/default/ui_extender.css', '//css/default/js-examples.css')
 
 );
